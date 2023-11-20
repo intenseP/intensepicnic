c9c56d5fce4cc8a4951fd4c2cd2c1f8cee33ec72
intenseP <125215207+intenseP@users.noreply.github.com>
2023-11-20 20:32:29 +0000

    Add files via upload

---
+++ MainGScript.js
@@ -0,0 +1,15 @@
+
+var website = "https://intensepicnic.netlify.app/files/"; 
+
+function goToITC(){
+  window.open(website + "into%20the%20catacombs%20files/into%20the%20catacombs", '_blank').focus();
+}
+function goToCrackhead(){
+  window.open(website + "crackheadfiles/crackhead", '_blank').focus();
+}
+function goToSmashTD(){
+  window.open(website + "smashtd%20files/smashtd", '_blank').focus();
+}
+function goTo20LOH(){
+  window.open(website + "20loh%20files/20loh", '_blank').focus();
+}
