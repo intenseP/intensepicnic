9a5132830a714f224457a1daced26217b9cf5d5a
intenseP <125215207+intenseP@users.noreply.github.com>
2024-03-01 07:45:17 +0000

    Add files via upload

--- MainGScript.js
+++ MainGScript.js
@@ -1,9 +1,7 @@
 
-//var website = "https://intensepicnic.netlify.app/files/"; 
-//for github: 
-var website = "https://intensep.github.io/intensepicnic/files/";
+var website = "https://intensepicnic.netlify.app/files/"; 
+//for github: website = "https://intensep.github.io/intensepicnic/files/";
 
-/*
 function goToITC(){
   window.open(website + "into%20the%20catacombs%20files/into%20the%20catacombs", '_blank').focus();
 }
@@ -16,7 +14,9 @@ function goToSmashTD(){
 function goTo20LOH(){
   window.open(website + "20loh%20files/20loh", '_blank').focus();
 }
-*/
+
+//for github:
+/*
 
 function goToITC(){
   window.open(website + "Into%20The%20Catacombs%20Files/Into%20The%20Catacombs", '_blank').focus();
@@ -30,3 +30,5 @@ function goToSmashTD(){
 function goTo20LOH(){
   window.open(website + "20LOH%20Files/20LOH", '_blank').focus();
 }
+
+*/
