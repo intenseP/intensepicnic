b058fd0fc0d24dbc6652f975874f4ff257a3dc7f
intenseP <125215207+intenseP@users.noreply.github.com>
2024-03-02 21:26:05 +0000

    Update MainGScript.js

--- MainGScript.js
+++ MainGScript.js
@@ -1,22 +1,5 @@
+var website = "https://intensep.github.io/intensepicnic/files/";
 
-var website = "https://intensepicnic.netlify.app/files/"; 
-//for github: website = "https://intensep.github.io/intensepicnic/files/";
-
-function goToITC(){
-  window.open(website + "into%20the%20catacombs%20files/into%20the%20catacombs", '_blank').focus();
-}
-function goToCrackhead(){
-  window.open(website + "crackheadfiles/crackhead", '_blank').focus();
-}
-function goToSmashTD(){
-  window.open(website + "smashtd%20files/smashtd", '_blank').focus();
-}
-function goTo20LOH(){
-  window.open(website + "20loh%20files/20loh", '_blank').focus();
-}
-
-//for github:
-/*
 
 function goToITC(){
   window.open(website + "Into%20The%20Catacombs%20Files/Into%20The%20Catacombs", '_blank').focus();
@@ -30,5 +13,3 @@ function goToSmashTD(){
 function goTo20LOH(){
   window.open(website + "20LOH%20Files/20LOH", '_blank').focus();
 }
-
-*/
