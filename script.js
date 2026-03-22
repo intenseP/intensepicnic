88bbf80789d6a229c2e3ba9a6e6058f73ba33467
intenseP <125215207+intenseP@users.noreply.github.com>
2026-03-22 00:47:56 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 var gameLocation = "/";
 
+function serverStartUp() {
+	console.log("ballsack");
+
+    const { exec } = require("node:child_process");
+
+	console.log("ballsack2.0");
+
+	// run the `ls` command using exec
+	exec('ls ./', (err, output) => {
+		// once the command has completed, the callback function is called
+		if (err) {
+			// log and return if we encounter an error
+			console.error("could not execute command: ", err)
+			return
+		}
+		// log the output received from the command
+		console.log("Output: \n", output)
+	})
+}
+
 function goToGame(game){
 	window.open(game + ".html", '_blank').focus();
 }
